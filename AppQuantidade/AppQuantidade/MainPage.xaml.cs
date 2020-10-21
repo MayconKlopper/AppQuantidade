6d03bd6c3d3e400fc4d322d93f7a77c4f63967b4
mayconklopper <maycon_klopper@hotmail.com>
2020-10-21 17:26:49 -0300

    Implementação da lógica

--- AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -10,9 +10,26 @@ namespace AppQuantidade
 {
     public partial class MainPage : ContentPage
     {
+        private int quantidade;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Soma(object sender, EventArgs e)
+        {
+            this.quantidade++;
+            this.Quantidade.Text = this.quantidade.ToString();
+        }
+
+        private void Subtrai(object sender, EventArgs e)
+        {
+            if (quantidade > 0)
+            {
+                this.quantidade--;
+                this.Quantidade.Text = this.quantidade.ToString();
+            }
         }
     }
 }
